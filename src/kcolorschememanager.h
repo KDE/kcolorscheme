7849c9b3b458cf97d5196a6c1cab6aeaa57e9390
Julius Künzel <julius.kuenzel@kde.org>
2024-07-26 14:03:51 +0200

    Deprecate saveSchemeToConfigFile()
    Advise to use saveSchemeIdToConfigFile() instead

--- src/kcolorschememanager.h
+++ src/kcolorschememanager.h
@@ -95,13 +95,21 @@ public:
      */
     QModelIndex indexForScheme(const QString &name) const;
 
+#if KCOLORSCHEME_ENABLE_DEPRECATED_SINCE(6, 17)
+
     /*!
      * Saves the color scheme to config file. The scheme is saved by default whenever it's changed.
      * Use this method when autosaving is turned off, see setAutosaveChanges().
      *
      * \since 5.89
+     *
+     * \deprecated[6.17]
+     * Use saveSchemeIdToConfigFile() instead which is not local dependend
      */
+    KCOLORSCHEME_DEPRECATED_VERSION(6, 17, "Use saveSchemeIdToConfigFile() instead which is not local dependend")
     void saveSchemeToConfigFile(const QString &schemeName) const;
+#endif
+
      /*!
       * Saves the color scheme to config file. The scheme is saved by default whenever it's changed.
       * Use this method when autosaving is turned off, see setAutosaveChanges().
